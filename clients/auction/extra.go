b75aa70c9f60a2b105ecba9579610b69d170ba3c
Slavomir <gagliardetto@users.noreply.github.com>
2021-09-15 22:19:44 +0200

    auction: add extra.go with constants

---
+++ clients/auction/extra.go
@@ -0,0 +1,8 @@
+package auction
+
+const BASE_AUCTION_DATA_SIZE = 32 + 32 + 9 + 9 + 9 + 9 + 1 + 32 + 1 + 8 + 8 + 8
+const BID_LENGTH = 32 + 8
+const MAX_AUCTION_DATA_EXTENDED_SIZE = 8 + 9 + 2 + 200
+const BIDDER_METADATA_LEN = 32 + 32 + 8 + 8 + 1
+const PREFIX = "auction"
+const EXTENDED = "extended"
